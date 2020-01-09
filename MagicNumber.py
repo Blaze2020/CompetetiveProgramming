72e890ff2aa8c8ac608364cdb08c26b8ce8408e1
Blaze2020 <karanam.kalpesh98@gmail.com>
2020-01-09 22:52:36 +0530

    "1st commit"

---
+++ MagicNumber.py
@@ -0,0 +1,17 @@
+#To find the magic no
+
+def calculate(str1):
+    a=list(str1)
+    for i in range(len(a)):
+        b=ord(a[i])
+        print(b)
+
+
+t=int(input())
+for i in range(t):
+    n=int(input())
+    str1=input()
+    if len(str1)==n:
+        calculate(str1)
+    else:
+        print("limit exceeded")
