b5f23f59296d2032c5f7d8f913f1665100c586db
Blaze2020 <karanam.kalpesh98@gmail.com>
2020-01-12 20:01:56 +0530

    "HackerEarth 2"

---
+++ Prasunthedetective.py
@@ -0,0 +1,30 @@
+'''
+Prasun the detective
+Prasun, a wanna be detective, has been assigned a case after his continuous failure at solving any. This is the murder
+of Snehashis who was killed last night. Prasun has already made some guesses on who could be the murderer and it seems that
+the two murderers are communicating via some strange arrangement of words. As prasun has been involved at solving cases of
+such fashion, he is making some guesses on what the message could probably be. Though he is pretty smart (:P), you have to
+help him by checking whether his guess is actually a possible one from the messages received.
+
+
+Constraints:
+
+Length of both message is less than 50 character.
+
+Line 1: Message Received.
+Line 2: Prasun's Guess.
+
+SAMPLE INPUT
+jogod #! siara.
+raja is good!
+SAMPLE OUTPUT
+YES
+Time Limit:	1.0 sec(s) for each input file.
+Memory Limit:	256 MB
+Source Limit:	1024 KB
+'''
+Line1="jogod #! siara."
+Line2=""
+Line3="raja is good!"
+print(list(Line1))
+print(list(Line3))
